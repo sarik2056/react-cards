e6e2265dcf3362afeb715c2fe10c449a5aaf7b23
sarik2056 <104066433+sarik2056@users.noreply.github.com>
2025-05-19 14:44:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import ProductTab from "./ProductTab.jsx";
+function App(){
+    return <>
+    <h2>BlockBuster Deals | Shop Now</h2>
+    <ProductTab />
+    </>
+}
+export default App;
